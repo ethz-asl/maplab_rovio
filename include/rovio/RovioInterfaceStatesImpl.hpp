ba4be39b3804315fa2607071e5cd71f24d2c430c
Marcin Dymczyk <marcin.dymczyk@mavt.ethz.ch>
2017-12-05 13:33:08 +0100

    get max num features

--- include/rovio/RovioInterfaceStatesImpl.hpp
+++ include/rovio/RovioInterfaceStatesImpl.hpp
@@ -141,6 +141,11 @@ struct RovioFeatureStateImpl : public RovioFeatureState {
     return status_vec[feature_idx];
   }
 
+  int get_MaxNumFeatures() const {
+    return kMaxNumFeatures;
+  }
+
+
   // Since we use a fixed number of features, the (in)valid ones are determined
   // with this array.
   bool isFeatureValid[kMaxNumFeatures];
