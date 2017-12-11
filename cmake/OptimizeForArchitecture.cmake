5483da5d677fe7e6a8caa2c1f38cc4e015def8e4
Fabian Blöchliger <f.bloechliger@gmail.com>
2017-12-11 12:53:56 +0100

    Added OptimizeForArchitecture.cmake script.

---
+++ cmake/OptimizeForArchitecture.cmake
@@ -0,0 +1,7 @@
+add_definitions(-std=c++11)
+execute_process(COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCH)
+if (ARCH MATCHES "^(arm)")
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -mfpu=neon")
+else()
+  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -mssse3")
+endif()
