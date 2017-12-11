ed827a7df80916082953711a6c61c3e7de14381f
Fabian Blöchliger <f.bloechliger@gmail.com>
2017-12-11 12:57:43 +0100

    Removed duplicate add_definitions(...).

--- cmake/OptimizeForArchitecture.cmake
+++ cmake/OptimizeForArchitecture.cmake
@@ -1,4 +1,3 @@
-add_definitions(-std=c++11)
 execute_process(COMMAND uname -m COMMAND tr -d '\n' OUTPUT_VARIABLE ARCH)
 if (ARCH MATCHES "^(arm)")
   set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11 -mfpu=neon")
